44251877021e3dbf11df90c4d00254ff5fa3b28e
PlamenHP <p.petrov.mail@gmail.com>
2016-08-01 20:17:21 +0100

    more exams

---
+++ C# Basics Exam 10 April 2014 Evening/Volleyball/Volleyball.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace Namespace
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Volleyball
+    {
+        static void Main()
+        {
+            string t = Console.ReadLine().Trim();
+            int holidays = int.Parse(Console.ReadLine());
+            int weekendsInHometow = int.Parse(Console.ReadLine());
+
+            decimal normalPlays = (48 - weekendsInHometow )* 3m / 4m;
+            decimal holidayPlays = (holidays * 2m) / 3m;
+            decimal hometownPlays = weekendsInHometow;
+
+            decimal totalPlays = normalPlays + holidayPlays + hometownPlays;
+            if (t == "leap")
+            {
+                totalPlays *= 1.15m;
+            }
+
+            Console.WriteLine(Math.Round(totalPlays));
+        }
+    }
+}
