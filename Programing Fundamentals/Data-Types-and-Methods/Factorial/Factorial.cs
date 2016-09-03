6f73e0cf1d52c04a16f0d67deb306cb4b1e4086c
PlamenHP <p.petrov.mail@gmail.com>
2016-09-03 12:48:45 +0300

    tasks from arrays topic completed

--- Programing Fundamentals/Data-Types-and-Methods/Factorial/Factorial.cs
+++ Programing Fundamentals/Data-Types-and-Methods/Factorial/Factorial.cs
@@ -3,13 +3,21 @@
 namespace Namespace
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
+    using System.Numerics;
 
-    class Program
+    class Factorial
     {
         static void Main()
         {
+            int n = int.Parse(Console.ReadLine());
+
+            BigInteger factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine(factorial);
         }
     }
 }
