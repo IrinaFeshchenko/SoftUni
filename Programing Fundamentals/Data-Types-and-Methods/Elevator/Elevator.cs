38aaeb87005b3162fa88b461cca7059562c33709
PlamenHP <p.petrov.mail@gmail.com>
2016-09-03 08:50:28 +0300

    more tasks

--- Programing Fundamentals/Data-Types-and-Methods/Elevator/Elevator.cs
+++ Programing Fundamentals/Data-Types-and-Methods/Elevator/Elevator.cs
@@ -6,10 +6,16 @@ namespace Namespace
     using System.Collections.Generic;
     using System.Linq;
 
-    class Program
+    class Elevator
     {
         static void Main()
         {
+            int persons = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+            int result = (int)Math.Ceiling((double)persons / capacity);
+
+            Console.WriteLine(result);
         }
     }
 }
