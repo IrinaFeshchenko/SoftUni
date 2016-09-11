9b5d909b5828a31a3388de4b2c72186dc4e63978
PlamenHP <p.petrov.mail@gmail.com>
2016-09-12 00:23:56 +0100

    more tasks

---
+++ Programing Fundamentals/ListsAndMatrices/SquaresInMatrix/SquaresInMatrix.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace Namespace
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class SquaresInMatrix
+    {
+        static void Main()
+        {
+            int[] param = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rows = param[0];
+            int cols = param[1];
+
+            for (int i = 0; i < length; i++)
+            {
+
+            }
+        }
+    }
+}
