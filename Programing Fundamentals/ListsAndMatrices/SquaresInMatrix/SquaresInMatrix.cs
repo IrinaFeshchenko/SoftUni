58b30e55d1f42c7133e9acc2c7adeb6ae193ec1f
PlamenHP <p.petrov.mail@gmail.com>
2016-09-12 00:29:06 +0100

    finished squares in matrix task

--- Programing Fundamentals/ListsAndMatrices/SquaresInMatrix/SquaresInMatrix.cs
+++ Programing Fundamentals/ListsAndMatrices/SquaresInMatrix/SquaresInMatrix.cs
@@ -13,11 +13,30 @@ namespace Namespace
             int[] param = Console.ReadLine().Split().Select(int.Parse).ToArray();
             int rows = param[0];
             int cols = param[1];
+            int[,] m = new int[rows, cols];
 
-            for (int i = 0; i < length; i++)
+            for (int i = 0; i < rows; i++)
             {
-
+                char[] num = Console.ReadLine().Split().Select(char.Parse).ToArray();
+                for (int j = 0; j < cols; j++)
+                {
+                    m[i, j] = num[j];
+                }
             }
+
+            int counter = 0;
+            for (int i = 0; i < rows - 1; i++)
+            {
+                for (int j = 0; j < cols - 1; j++)
+                {
+                    if ((m[i, j] == m[i, j + 1]) && (m[i, j] == m[i + 1, j]) && (m[i, j] == m[i + 1, j + 1]))
+                    {
+                        counter++;
+                    }
+                }
+            }
+
+            Console.WriteLine(counter);
         }
     }
 }
