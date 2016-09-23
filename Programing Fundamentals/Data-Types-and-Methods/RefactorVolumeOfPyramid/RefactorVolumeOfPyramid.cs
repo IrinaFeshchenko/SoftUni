35ca4b6278d8f5037c48a789b4efb0fe5bd86bbf
PlamenHP <p.petrov.mail@gmail.com>
2016-09-23 23:55:59 +0100

    more tasks

--- Programing Fundamentals/Data-Types-and-Methods/RefactorVolumeOfPyramid/RefactorVolumeOfPyramid.cs
+++ Programing Fundamentals/Data-Types-and-Methods/RefactorVolumeOfPyramid/RefactorVolumeOfPyramid.cs
@@ -16,7 +16,7 @@ namespace Namespace
             double width = double.Parse(Console.ReadLine());
             Console.Write("Height: ");
             double height = double.Parse(Console.ReadLine());
-            double volume = (lenght + width + height) / 3;
+            double volume = (lenght * width * height) / 3;
             Console.WriteLine("Pyramid Volume: {0:F2}", volume);
         }
     }
---
+++ Programing Fundamentals/MethodsAndDebugging/LongerLine/LongerLine.cs
@@ -0,0 +1,54 @@
+﻿
+namespace Namespace
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    class Point
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+    }
+    class CenterPrint
+    {
+        static void Main()
+        {
+
+            Point firstPoint = new Point();
+            firstPoint.x = double.Parse(Console.ReadLine());
+            firstPoint.y = double.Parse(Console.ReadLine());
+            Point secondPoint = new Point();
+            secondPoint.x = double.Parse(Console.ReadLine());
+            secondPoint.y = double.Parse(Console.ReadLine());
+            PrintThePoinCloserToZero(firstPoint, secondPoint);
+        }
+
+        private static void PrintThePoinCloserToZero(Point firstPoint, Point secondPoint)
+        {
+            double firstDistance = GetDistance(firstPoint);
+            double secondDistance = GetDistance(secondPoint);
+
+            if (firstDistance <= secondDistance)
+            {
+                Print(firstPoint);
+            }
+            else
+            {
+                Print(secondPoint);
+            }
+        }
+
+        private static void Print(Point p)
+        {
+            Console.WriteLine($"({p.x}, {p.y})");
+        }
+
+        private static double GetDistance(Point p)
+        {
+            double distance = Math.Sqrt(p.x * p.x + p.y * p.y);
+            return distance;
+        }
+
+
+    }
+}
