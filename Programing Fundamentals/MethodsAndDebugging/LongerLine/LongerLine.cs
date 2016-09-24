b7de14bd6a9df8dadc9f7e7c322af6204b074225
PlamenHP <p.petrov.mail@gmail.com>
2016-09-24 20:57:10 +0100

    another task

--- Programing Fundamentals/MethodsAndDebugging/LongerLine/LongerLine.cs
+++ Programing Fundamentals/MethodsAndDebugging/LongerLine/LongerLine.cs
@@ -1,54 +1,122 @@
 ﻿
-namespace Namespace
+namespace LongerLine
 {
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    class Point
+
+    public class Point
     {
         public double x { get; set; }
         public double y { get; set; }
-    }
-    class CenterPrint
-    {
-        static void Main()
-        {
 
-            Point firstPoint = new Point();
-            firstPoint.x = double.Parse(Console.ReadLine());
-            firstPoint.y = double.Parse(Console.ReadLine());
-            Point secondPoint = new Point();
-            secondPoint.x = double.Parse(Console.ReadLine());
-            secondPoint.y = double.Parse(Console.ReadLine());
-            PrintThePoinCloserToZero(firstPoint, secondPoint);
+        public Point()
+        {
         }
 
-        private static void PrintThePoinCloserToZero(Point firstPoint, Point secondPoint)
+        public Point(double x, double y)
         {
-            double firstDistance = GetDistance(firstPoint);
-            double secondDistance = GetDistance(secondPoint);
+            this.x = x;
+            this.y = y;
+        }
 
-            if (firstDistance <= secondDistance)
+        public double GetDistanceToCentre()
+        {
+            double distance = Math.Sqrt(this.x * this.x + this.y * this.y);
+            return distance;
+        }
+    }
+
+    public class Line : IComparable
+    {
+        public Point startPoint { get; set; }
+        public Point endPoint { get; set; }
+
+        public Line()
+        {
+        }
+
+        public Line(Point start, Point end)
+        {
+            this.startPoint = start;
+            this.endPoint = end;
+        }
+
+        public int CompareTo(object obj)
+        {
+            Line secondLine = (Line)obj;
+            double thisLineLenght = this.GetLenght();
+            double secondLineLenght = secondLine.GetLenght();
+
+            if (thisLineLenght < secondLineLenght)
             {
-                Print(firstPoint);
+                return -1;
+            }
+            else if (thisLineLenght > secondLineLenght)
+            {
+                return 1;
             }
             else
             {
-                Print(secondPoint);
+                return 0;
             }
         }
 
-        private static void Print(Point p)
+        public double GetLenght()
         {
-            Console.WriteLine($"({p.x}, {p.y})");
+            double x1 = this.startPoint.x;
+            double x2 = this.endPoint.x;
+            double y1 = this.startPoint.y;
+            double y2 = this.endPoint.y;
+
+            double lenght = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            return Math.Abs(lenght);
+        }
+    }
+
+    class LongerLine
+    {
+        static void Main()
+        {
+            Line firstLine = ReadLine();
+            Line secondLine = ReadLine();
+
+            if (firstLine.CompareTo(secondLine) >= 0)
+            {
+                Print(firstLine);
+            }
+            else
+            {
+                Print(secondLine);
+            }
         }
 
-        private static double GetDistance(Point p)
+        private static Line ReadLine()
         {
-            double distance = Math.Sqrt(p.x * p.x + p.y * p.y);
-            return distance;
+            Line line = new Line();
+            line.startPoint = new Point();
+            line.startPoint.x = double.Parse(Console.ReadLine());
+            line.startPoint.y = double.Parse(Console.ReadLine());
+            line.endPoint = new Point();
+            line.endPoint.x = double.Parse(Console.ReadLine());
+            line.endPoint.y = double.Parse(Console.ReadLine());
+            return line;
         }
 
 
+        private static void Print(Line line)
+        {
+            Point p1 = new Point(line.startPoint.x, line.startPoint.y);
+            Point p2 = new Point(line.endPoint.x, line.endPoint.y);
+
+            if (p1.GetDistanceToCentre() < p2.GetDistanceToCentre())
+            {
+                Console.WriteLine($"({p1.x}, {p1.y})({p2.x}, {p2.y})");
+            }
+            else
+            {
+                Console.WriteLine($"({p2.x}, {p2.y})({p1.x}, {p1.y})");
+            }
+        }
     }
 }
