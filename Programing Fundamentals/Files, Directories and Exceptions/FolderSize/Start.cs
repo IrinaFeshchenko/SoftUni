7ef12c09c5c264a3eec42b38472e702783ee2b1e
PlamenHP <p.petrov.mail@gmail.com>
2016-10-14 00:29:50 +0100

    task finished

--- Programing Fundamentals/Files, Directories and Exceptions/FolderSize/Start.cs
+++ Programing Fundamentals/Files, Directories and Exceptions/FolderSize/Start.cs
@@ -1,15 +1,29 @@
-﻿
-
-namespace Namespace
+﻿namespace FolderSize
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
 
-    class Program
+    public class Start
     {
-        static void Main()
+        public static void Main()
         {
+            string folder = "TestFolder";
+            string resultFile = "result.txt";
+
+            var files = Directory.GetFiles(folder);
+
+            double sum = 0;
+            foreach (var file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                sum += fileInfo.Length;
+            }
+
+            sum = sum / 1024 / 1024;
+
+            File.WriteAllText(resultFile, sum.ToString());
         }
     }
 }
