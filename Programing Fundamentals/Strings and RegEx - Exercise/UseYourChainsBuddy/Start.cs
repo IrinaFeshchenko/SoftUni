3833b58bd4189bc117dd4db248f67e4749a76f76
PlamenHP <p.petrov.mail@gmail.com>
2016-10-22 00:37:23 +0300

    task finished

--- Programing Fundamentals/Strings and RegEx - Exercise/UseYourChainsBuddy/Start.cs
+++ Programing Fundamentals/Strings and RegEx - Exercise/UseYourChainsBuddy/Start.cs
@@ -10,8 +10,8 @@
     {
         public static void Main()
         {
-            Console.WriteLine('n'-'a');
-            string text = "<html><head><title></title></head><body><h1>Intro</h1><ul><li>Item01</li><li>Item02</li><li>Item03</li></ul><p>jura qevivat va jrg fyvccrel fabjl</p><div><button>Click me, baby!</button></div><p> pbaqvgvbaf fabj  qpunvaf ner nofbyhgryl rffragvny sbe fnsr unaqyvat nygubhtu fabj punvaf znl ybbx </p><span>This manual is false, do not trust it! The illuminati wrote it down to trick you!</span><p>vagvzvqngvat gur onfvp vqrn vf ernyyl fvzcyr svg gurz bire lbhe gverf qevir sbejneq fybjyl naq gvtugra gurz hc va pbyq jrg</p><p> pbaqvgvbaf guvf vf rnfvre fnvq guna qbar ohg vs lbh chg ba lbhe gverf</p></body>";
+            //string text = "<html><head><title></title></head><body><h1>Intro</h1><ul><li>Item01</li><li>Item02</li><li>Item03</li></ul><p>jura qevivat va jrg fyvccrel fabjl</p><div><button>Click me, baby!</button></div><p> pbaqvgvbaf fabj  qpunvaf ner nofbyhgryl rffragvny sbe fnsr unaqyvat nygubhtu fabj punvaf znl ybbx </p><span>This manual is false, do not trust it! The illuminati wrote it down to trick you!</span><p>vagvzvqngvat gur onfvp vqrn vf ernyyl fvzcyr svg gurz bire lbhe gverf qevir sbejneq fybjyl naq gvtugra gurz hc va pbyq jrg</p><p> pbaqvgvbaf guvf vf rnfvre fnvq guna qbar ohg vs lbh chg ba lbhe gverf</p></body>";
+            string text = Console.ReadLine();
             string pattern = @"<p>(.+?)<\/p>";
             var tags = Regex.Matches(text, pattern);
 
@@ -22,8 +22,7 @@
                 sb.Append(t.Groups[1].ToString());
             }
 
-
-            char[] line = Regex.Replace(sb.ToString(), @"[^a-z0-9]|\s+|\n", " ").ToCharArray();
+            char[] line = Regex.Replace(sb.ToString(), @"[^a-z0-9]", " ").ToCharArray();
 
             for (int i = 0; i < line.Length; i++)
             {
@@ -40,9 +39,8 @@
                 }
             }
 
-            string s = string.Join("", line);
-            string result = Regex.Replace(s, @"\s+|\n", " ").Trim();
-            Console.WriteLine(string.Join(" ", result));
+            string result = Regex.Replace(string.Join("", line), @"\s+", " ").Trim();
+            Console.WriteLine(result);
         }
     }
 }
