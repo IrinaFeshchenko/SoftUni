fa759b024f9382d56b0708df386ac7a33f39fa51
PlamenHP <p.petrov.mail@gmail.com>
2016-10-21 17:33:49 +0300

    task finished

--- Programing Fundamentals/Strings and RegEx - Exercise/MelrahShake/Start.cs
+++ Programing Fundamentals/Strings and RegEx - Exercise/MelrahShake/Start.cs
@@ -1,42 +1,30 @@
 ﻿namespace MelrahShake
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Text.RegularExpressions;
 
     public static class Start
     {
         public static void Main()
         {
-            Console.WriteLine(2/2);
             string text = Console.ReadLine();
             string pattern = Console.ReadLine();
-            
+
             while (true)
             {
                 int first = text.IndexOf(pattern, 0);
                 int last = text.LastIndexOf(pattern);
 
-                if (first!=last)
+                if (first != last)
                 {
                     text = text.Remove(last, pattern.Length);
                     text = text.Remove(first, pattern.Length);
 
                     Console.WriteLine("Shaked it.");
 
-                    if (pattern.Length < 3)
+                    if (pattern.Length < 2)
                     {
-                        if (pattern.Length == 2)
-                        {
-                            pattern = pattern[0].ToString();
-                        }
-                        else if (pattern.Length == 1)
-                        {
-                            Finish(text);
-                            break;
-                        }
+                        Finish(text);
+                        break;
                     }
                     else
                     {
