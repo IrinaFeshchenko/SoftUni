75df5b67cfedd7dca3fa4db8474118ad41a3861c
PlamenHP <p.petrov.mail@gmail.com>
2016-10-21 15:20:59 +0300

    task finished

---
+++ Programing Fundamentals/Strings and RegEx - Exercise/MelrahShake/Start.cs
@@ -0,0 +1,59 @@
+﻿namespace MelrahShake
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public static class Start
+    {
+        public static void Main()
+        {
+            string text = Console.ReadLine();
+            string pattern = Console.ReadLine();
+            
+            while (true)
+            {
+                int first = text.IndexOf(pattern, 0);
+                int last = text.LastIndexOf(pattern);
+
+                if (first!=last)
+                {
+                    text = text.Remove(last, pattern.Length);
+                    text = text.Remove(first, pattern.Length);
+
+                    Console.WriteLine("Shake it.");
+
+                    if (pattern.Length < 3)
+                    {
+                        if (pattern.Length == 2)
+                        {
+                            pattern = pattern[1].ToString();
+                        }
+                        else if (pattern.Length == 1)
+                        {
+                            Finish(text);
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        pattern = pattern.Remove(pattern.Length / 2, 1);
+                    }
+                }
+                else
+                {
+                    Finish(text);
+                    break;
+                }
+            }
+        }
+
+        private static void Finish(string text)
+        {
+            Console.WriteLine("No shake");
+            Console.WriteLine(text);
+        }
+    }
+}
