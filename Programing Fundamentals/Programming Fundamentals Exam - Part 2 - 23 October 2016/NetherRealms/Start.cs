f1b92c6d479b5658b0f3f85e9f422858d45981e2
PlamenHP <p.petrov.mail@gmail.com>
2016-10-23 18:36:05 +0300

    task update

--- Programing Fundamentals/Programming Fundamentals Exam - Part 2 - 23 October 2016/NetherRealms/Start.cs
+++ Programing Fundamentals/Programming Fundamentals Exam - Part 2 - 23 October 2016/NetherRealms/Start.cs
@@ -3,94 +3,58 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Text.RegularExpressions;
+
     class Start
     {
         static void Main()
         {
-            string s = Console.ReadLine();
-
-            string[] args = s.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-            char[] c = "0123456789+-*/.".ToCharArray();
-
-
+            string input = Console.ReadLine();
+            string[] args = input.Split(new[] { ' ',',' }, StringSplitOptions.RemoveEmptyEntries);
+            char[] skipLetters = "0123456789+-*/.".ToCharArray();
             List<string> result = new List<string>();
-            foreach (var item in args)
+
+            foreach (var dragonName in args)
             {
-                item.TrimEnd(',');
                 long lettersSum = 0;
 
-                foreach (var ch in item)
+                foreach (var letter in dragonName)
                 {
-                    if(!c.Contains(ch))
-                    //if (char.IsLetter(ch))
+                    if(!skipLetters.Contains(letter))
                     {
-                        lettersSum += (long)ch;
+                        lettersSum += (long)letter;
                     }
                 }
 
-                string currentNumber = "";
-                decimal? lastDigit = null;
-                char operation = '+';
-                decimal sum = 0;
+                string numbersPattern = @"(-?\d*\.?\d+)";
+                var numbersMatches = Regex.Matches(dragonName,numbersPattern);
 
-                foreach (var ch in item)
+                decimal numbersSum = 0;
+                foreach (Match numberMatch in numbersMatches)
                 {
-                    long currentDigit = 0;
-                    if (char.IsDigit(ch))
-                    {
-                        currentNumber += ch;
-                    }
-                    else if (ch == '+')
-                    {
-                        operation = '+';
-                    }
-                    else if (ch == '-')
-                    {
-                        operation = '-';
-                    }
-                    else if (ch == '.')
-                    {
-                        currentNumber += '.';
-                    }
-                    else
-                    {
-                        if (currentNumber != "")
-                        {
-                            if (operation == '+')
-                            {
-                                sum += decimal.Parse(currentNumber);
-                                currentNumber = "";
-                            }
-                            else if (operation == '-')
-                            {
-                                sum -= decimal.Parse(currentNumber);
-                                operation = '+';
-                                currentNumber = "";
-                            }
-                        }
+                    numbersSum += decimal.Parse(numberMatch.Groups[1].Value);
+                }
 
+                foreach (var letter in dragonName)
+                {
+                    if (letter == '*')
+                    {
+                        numbersSum *= 2;
+                    }
+                    if (letter == '/')
+                    {
+                        numbersSum /= 2;
                     }
                 }
 
-                foreach (var ch in item)
-                {
-                    if (ch == '*')
-                    {
-                        sum *= 2;
-                    }
-                    if (ch == '/')
-                    {
-                        sum /= 2;
-                    }
-                }
-
-                result.Add($"{item.Trim(',')} - {lettersSum} health, {sum:f2} damage");
+                result.Add($"{dragonName} - {lettersSum} health, {numbersSum:f2} damage");
             }
 
-            var r = result.OrderBy(x=>x);
-            foreach (var item in r)
+            var sortedResult = result.OrderBy(x=>x);
+
+            foreach (var dragonInfo in sortedResult)
             {
-                Console.WriteLine(item);
+                Console.WriteLine(dragonInfo);
             }
         }
     }
