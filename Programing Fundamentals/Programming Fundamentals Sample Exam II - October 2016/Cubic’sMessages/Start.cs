1d24f28ac879a35088703e1868fe4bf3c64c8a4a
PlamenHP <p.petrov.mail@gmail.com>
2016-10-27 13:40:50 -0100

    task finished

--- Programing Fundamentals/Programming Fundamentals Sample Exam II - October 2016/Cubic’sMessages/Start.cs
+++ Programing Fundamentals/Programming Fundamentals Sample Exam II - October 2016/Cubic’sMessages/Start.cs
@@ -15,16 +15,45 @@
             while ((input = Console.ReadLine()) != "Over!")
             {
                 int n = int.Parse(Console.ReadLine());
-                string pattern = $"(^\\d+)([A-Za-z]{{{n}}})(.*)";
+                string pattern = @"^(\d+)([A-Za-z]{" + n + "})([^a-zA-Z]*)$";
 
                 if (Regex.IsMatch(input, pattern))
                 {
-                    var message = Regex.Match(input,pattern).Groups[1].Value;
-                    var part = Regex.Match(input,pattern).Groups[2].Value;
-
+                    var match = Regex.Match(input, pattern);
+                    var message = match.Groups[2].Value;
+                    var part1 = match.Groups[1].Value;
+                    var part2 = string.Join("",match.Groups[3].Value.Where(x=>char.IsDigit(x)));
+                    var indexes = part1 + part2;
                     messages.Add(message);
+
+                    string result = string.Empty;
+                    foreach (var index in indexes.Select(x=>int.Parse(x.ToString())))
+                    {
+                        if (isValidIndex(index,message.Length))
+                        {
+                            result += message[index];
+                        }
+                        else
+                        {
+                            result += " ";
+                        }
+                    }
+
+                    Console.WriteLine($"{message} == {result}");
                 }
             }
+        }
+
+        private static bool isValidIndex(int index, int len)
+        {
+            bool isValidIndex = true;
+
+            if (index<0 || index > len )
+            {
+                isValidIndex = false;
+            }
+
+            return isValidIndex;
         }
     }
 }
