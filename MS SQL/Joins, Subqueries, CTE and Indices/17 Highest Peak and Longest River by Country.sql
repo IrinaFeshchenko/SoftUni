dcae9f4c340742f0605d98e602c40d546c4be2aa
PlamenHP <p.petrov.mail@gmail.com>
2017-02-13 00:23:11 +0000

    queries added

--- MS SQL/Joins, Subqueries, CTE and Indices/17 Highest Peak and Longest River by Country.sql
+++ MS SQL/Joins, Subqueries, CTE and Indices/17 Highest Peak and Longest River by Country.sql
@@ -1,24 +1,12 @@
 USE Geography
 GO
 
-SELECT TOP 5 Peaks.CountryName, Peaks.maxHeigth AS HighestPeakElevation, Rivers.maxLength AS LongestRiverLength
-FROM
-
-(
-	SELECT c.CountryName, MAX(p.Elevation) AS maxHeigth
-	FROM Countries c
-	LEFT JOIN MountainsCountries mc ON c.CountryCode = mc.CountryCode
-	LEFT JOIN Mountains m ON m.Id = mc.MountainId
-	LEFT JOIN Peaks p ON p.MountainId = m.Id
-	GROUP BY c.CountryName
-) AS Peaks
-INNER JOIN
-(
-	SELECT c.CountryName, MAX(r.Length) AS maxLength
-	FROM Countries c
-	LEFT JOIN CountriesRivers cr ON c.CountryCode = cr.CountryCode
-	LEFT JOIN Rivers r ON r.Id = cr.RiverId
-	GROUP BY c.CountryName
-) AS Rivers
-ON Peaks.CountryName = Rivers.CountryName
-ORDER BY Peaks.maxHeigth DESC, Rivers.maxLength DESC, Peaks.CountryName ASC
+SELECT TOP 5 c.CountryName, MAX(p.Elevation)AS HighestPeakElevation, MAX(r.Length) AS LongestRiverLength
+FROM Countries c
+LEFT JOIN MountainsCountries mc ON c.CountryCode = mc.CountryCode
+LEFT JOIN Mountains m ON m.Id = mc.MountainId
+LEFT JOIN Peaks p ON p.MountainId = m.Id
+LEFT JOIN CountriesRivers cr ON c.CountryCode = cr.CountryCode
+LEFT JOIN Rivers r ON r.Id = cr.RiverId
+GROUP BY c.CountryName
+ORDER BY HighestPeakElevation DESC, LongestRiverLength DESC, c.CountryName ASC
