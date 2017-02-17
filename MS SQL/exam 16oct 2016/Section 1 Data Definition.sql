7b6b31170cecafe7ced00360f5b992a18058323d
PlamenHP <p.petrov.mail@gmail.com>
2017-02-17 02:39:17 +0000

    queries added

--- MS SQL/exam 16oct 2016/Section 1 Data Definition.sql
+++ MS SQL/exam 16oct 2016/Section 1 Data Definition.sql
@@ -1,13 +1,18 @@
-use Airport
-go
-
 create table Flights(
-FlightID int identity,
-DepartureTime date not null,
-ArrivalTime date not null,
-Status varchar(9) constraint chk_status check (Status in('Departing','Delayed', 'Arrived', 'Cancelled')),
-OriginAirportID int constraint fk_flights_airportsOrigin foreign key(OriginAirportID) references Airports(AirportID),
-DestinationAirportID int constraint fk_flights_airportsDestination foreign key(DestinationAirportID) references Airports(AirportID),
-AirlineID int constraint fk_flights_airlines foreign key(AirlineID) references Airlines(AirlineID),
+FlightID int primary key,
+DepartureTime datetime not null,
+ArrivalTime datetime not null,
+Status varchar(9) check (Status in('Departing','Delayed', 'Arrived', 'Cancelled')),
+OriginAirportID int foreign key references Airports(AirportID),
+DestinationAirportID int foreign key references Airports(AirportID),
+AirlineID int foreign key references Airlines(AirlineID),
 )
 
+create table Tickets(
+TicketID int primary key,
+Price decimal(8,2) not null,
+Class varchar(6) check (Class in ('First', 'Second', 'Third')),
+Seat varchar(5) not null,
+CustomerID int foreign key references Customers(CustomerID),
+FlightID int foreign key references Flights(FlightID)
+)
