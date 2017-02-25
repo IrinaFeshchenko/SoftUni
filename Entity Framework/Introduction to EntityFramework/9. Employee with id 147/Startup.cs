d6d145ab3aa8d2a4dd57fcdde35e24ae878105b9
PlamenHP <p.petrov.mail@gmail.com>
2017-02-25 17:41:02 +0200

    task completed

---
+++ Entity Framework/Introduction to EntityFramework/9. Employee with id 147/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.Employee_with_id_147
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
