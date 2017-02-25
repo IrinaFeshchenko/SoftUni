3f631ded15005ec924dab7740b63c1591fc73513
PlamenHP <p.petrov.mail@gmail.com>
2017-02-25 17:01:16 +0200

    task completed

---
+++ Entity Framework/Introduction to EntityFramework/8. Addresses by Town Name/Startup.cs
@@ -0,0 +1,16 @@
+﻿namespace _8.Addresses_by_Town_Name
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Startup
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
