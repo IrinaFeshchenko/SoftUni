68c63ef0d815638f9042551de338ea37096e523d
PlamenHP <p.petrov.mail@gmail.com>
2017-02-25 18:33:06 +0200

    task completed

--- Entity Framework/Introduction to EntityFramework/9. Employee with id 147/Startup.cs
+++ Entity Framework/Introduction to EntityFramework/9. Employee with id 147/Startup.cs
@@ -1,15 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _9.Employee_with_id_147
+﻿namespace _9.Employee_with_id_147
 {
-    class Program
+    using SoftUni_Database;
+    using System;
+    using System.Linq;
+
+    class Startup
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var context = new SoftUniContext();
+            var employee = context.Employees
+                .Where(e => e.EmployeeID == 147)
+                .Select(e=> new { e.FirstName, e.LastName, e.JobTitle, e.Projects})
+                .FirstOrDefault();
+
+            Console.WriteLine($"{employee.FirstName} {employee.LastName} {employee.JobTitle}");
+            foreach (var p in employee.Projects)
+            {
+                Console.WriteLine($"");
+            }
         }
     }
 }
