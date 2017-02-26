a72941b29aafd2c6663c1e838b46c1d961dc6dca
PlamenHP <p.petrov.mail@gmail.com>
2017-02-26 13:15:39 +0200

    task completed

---
+++ Entity Framework/Introduction to EntityFramework/14. First Letter/Startup.cs
@@ -0,0 +1,18 @@
+﻿namespace _14.First_Letter
+{
+    using Gringotts_Database;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Startup
+    {
+        static void Main()
+        {
+            var context = new GringottsContext();
+
+        }
+    }
+}
