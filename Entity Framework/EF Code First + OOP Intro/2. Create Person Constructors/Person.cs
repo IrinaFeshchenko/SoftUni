527db9ef6bef3815fea5a6f8c50ff8b99c944d94
PlamenHP <p.petrov.mail@gmail.com>
2017-03-02 00:47:05 +0000

    task completed

---
+++ Entity Framework/EF Code First + OOP Intro/2. Create Person Constructors/Person.cs
@@ -0,0 +1,35 @@
+﻿namespace _2.Create_Person_Constructors
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Person
+    {
+        public Person()
+            : this("No name", 1)
+        {
+        }
+
+        public Person(int age)
+            :this("No name",age)
+        {
+        }
+
+        public Person(string name)
+            : this(name, 1)
+        {
+        }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
