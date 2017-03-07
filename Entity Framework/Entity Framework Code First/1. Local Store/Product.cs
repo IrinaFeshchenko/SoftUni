dd27c0069f4e95fe7bc739bcf8484a41f96cb37b
PlamenHP <p.petrov.mail@gmail.com>
2017-03-07 23:28:44 +0000

    task completed

--- Entity Framework/Entity Framework Code First/1. Local Store/Product.cs
+++ Entity Framework/Entity Framework Code First/1. Local Store/Product.cs
@@ -3,9 +3,13 @@
     public class Product
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
+
         public string DistributorName { get; set; }
+
         public string Description { get; set; }
+
         public decimal Price { get; set; }
     }
 }
