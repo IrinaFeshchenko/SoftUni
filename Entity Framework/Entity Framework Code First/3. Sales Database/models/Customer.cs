00709308b36c2e84ec223b99f9d518c9f8a6ffe0
PlamenHP <p.petrov.mail@gmail.com>
2017-03-08 04:18:02 +0000

    task completed

---
+++ Entity Framework/Entity Framework Code First/3. Sales Database/models/Customer.cs
@@ -0,0 +1,22 @@
+﻿namespace _3.Sales_Database
+{
+    using System.Collections.Generic;
+
+    public class Customer
+    {
+        public Customer()
+        {
+        }
+
+        public Customer(string name, string email, string credidCardNumber)
+        {
+            SalesForCustomer = new HashSet<Sale>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string CreditCardNumber { get; set; }
+        public ICollection<Sale> SalesForCustomer { get; set; }
+    }
+}
