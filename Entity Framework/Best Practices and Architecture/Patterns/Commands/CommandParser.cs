717dfaf7e9e3204c39d1c5ff6dfad845f59fb551
PlamenHP <p.petrov.mail@gmail.com>
2017-03-21 23:59:37 +0000

    lecture task finished

---
+++ Entity Framework/Best Practices and Architecture/Patterns/Commands/CommandParser.cs
@@ -0,0 +1,36 @@
+﻿namespace Patterns.Commands
+{
+    using System.Collections.Generic;
+
+    class CommandParser
+    {
+        private Dictionary<string, Command> commands;
+
+        public CommandParser()
+        {
+            this.Initialize(null);
+        }
+
+        public Command Parse(string commandAsString, MyData data)
+        {
+            if (commands.ContainsKey(commandAsString))
+            {
+                return commands[commandAsString].Create(data);
+            }
+            else
+            {
+                return new NotFoundCommand(data);
+            }
+        }
+
+        private void Initialize(MyData data)
+        {
+            commands = new Dictionary<string, Command>();
+            commands.Add("bye",new PrintByeCommand(data));
+            commands.Add("greet", new PrintGreetingCommand(data));
+            commands.Add("exit", new ExitCommand(data));
+            commands.Add("increase", new IncreaseNumberCommand(data));
+            commands.Add("print", new PrintStringCommand(data));
+        }
+    }
+}
---
+++ Entity Framework/Best Practices and Architecture/Patterns/Startup.cs
@@ -0,0 +1,52 @@
+﻿
+namespace Patterns
+{
+    using Commands;
+    using Services;
+    using System;
+
+    class Startup
+    {
+        static void Main()
+        {
+            //RunServices();
+
+            MyData data = new MyData()
+            {
+                MyNumber = 5,
+                MyString = "Some data"
+            };
+
+            while (true)
+            {
+                RunCommands(data);
+            }
+        }
+
+        static void RunCommands(MyData data)
+        {
+
+
+            var parser = new CommandParser();
+            Console.WriteLine("Entercommand (greet,bye,exit,increase,print):");
+            var cmd = parser.Parse(Console.ReadLine(),data);
+
+            cmd.Execute();
+        }
+
+        static void RegisterServices()
+        {
+            Locator.Instance.AddService("console", new ConsoleLogger());
+            Locator.Instance.AddService("decorated", new DecoratedLogger());
+        }
+
+        static void RunServices()
+        {
+            string mystring = "Hello service locator!";
+            RegisterServices();
+
+            var logger = Locator.Instance.GetService("consolee");
+            logger.Log(mystring);
+        }
+    }
+}
