99d636a643eee1161499e80f2a2146bbb4394f10
PlamenHP <p.petrov.mail@gmail.com>
2017-03-25 12:58:26 +0000

    automapper simple mapping

---
+++ Entity Framework/Auto Mapping Objects/1. Simple Mapping/Startup.cs
@@ -0,0 +1,30 @@
+﻿
+namespace _1.Simple_Mapping
+{
+    using AutoMapper;
+    using System;
+    using System.Globalization;
+
+    class Startup
+    {
+        static void Main()
+        {
+            var employee = new Employee()
+            {
+                FirstName = "Bill",
+                LastName = "Gates",
+                Salary = 50000m,
+                Birthdate = DateTime.ParseExact("01/01/1960", "d/M/yyyy", CultureInfo.InvariantCulture),
+                Address = "Main str. Los Angeles"
+            };
+
+            Mapper.Initialize(cfg => cfg.CreateMap<Employee,EmployeeDto>());
+
+            var employeeDto = Mapper.Map<EmployeeDto>(employee);
+
+            Console.WriteLine(employeeDto.FirstName);
+            Console.WriteLine(employeeDto.LastName);
+            Console.WriteLine(employeeDto.Salary);
+        }
+    }
+}
