0dee719bf293cc8bec9026b498bdcee42afc52e6
PlamenHP <p.petrov.mail@gmail.com>
2017-03-27 00:28:37 +0100

    Products Shop DB added

---
+++ Entity Framework/JSON Processing/1. Products Shop/Startup.cs
@@ -0,0 +1,12 @@
+﻿
+namespace _1.Products_Shop
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            var context = new ProductsShopDBContext();
+            context.Database.Initialize(true);
+        }
+    }
+}
