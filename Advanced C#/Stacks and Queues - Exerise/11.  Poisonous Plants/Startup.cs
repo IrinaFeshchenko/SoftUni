7c2ec952854377871fc5bc7192dadd5f345cda70
PlamenHP <p.petrov.mail@gmail.com>
2017-05-27 22:45:06 +0100

    tasks completed

---
+++ Advanced C#/Stacks and Queues - Exerise/11.  Poisonous Plants/Startup.cs
@@ -0,0 +1,14 @@
+﻿
+namespace _11.Poisonous_Plants
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Startup
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
