b3396f2c4a640ff20277cd9a7a9a691ae81f1e74
PlamenHP <p.petrov.mail@gmail.com>
2017-05-29 23:31:41 +0100

    tasks added

---
+++ Advanced C#/Sets and Dictionaries - Excercise/8. Hands of cards/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.Hands_of_cards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
