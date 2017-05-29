dcc774c845adf758d9369e43b4883f60efafa755
PlamenHP <p.petrov.mail@gmail.com>
2017-05-29 08:05:30 +0100

    task completed

---
+++ Advanced C#/Sets and Dictionaries - Excercise/3. Periodic Table/Program.cs
@@ -0,0 +1,30 @@
+﻿
+namespace _3.Periodic_Table
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            SortedSet<string> chemElements = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] args = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var arg in args)
+                {
+                    chemElements.Add(arg);
+                }
+            }
+
+            foreach (var element in chemElements)
+            {
+                Console.Write($"{element} ");
+            }
+        }
+    }
+}
