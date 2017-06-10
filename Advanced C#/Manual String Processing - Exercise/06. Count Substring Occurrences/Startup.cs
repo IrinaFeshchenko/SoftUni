164fe9388de79bdb9a9fa3b13e031064c758344c
PlamenHP <p.petrov.mail@gmail.com>
2017-06-10 21:00:10 +0100

    task completed

---
+++ Advanced C#/Manual String Processing - Exercise/06. Count Substring Occurrences/Startup.cs
@@ -0,0 +1,24 @@
+﻿namespace _06.Count_Substring_Occurrences
+{
+    using System;
+
+    public class Startup
+    {
+        static void Main()
+        {
+            string text = "Welcome to the Software University (SoftUni)! Welcome to programming. Programming is wellness for developers, said Maxwell.";
+            string word = "wel";
+
+            int index = 0;
+            int count = 0;
+
+            while ((index = text.IndexOf(word, index,StringComparison.CurrentCultureIgnoreCase)) != -1)
+            {
+                count++;
+                index++;
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
