0d50df22b305a7f1319aefc7084bde2f61cc70dd
PlamenHP <p.petrov.mail@gmail.com>
2017-06-11 18:59:10 +0100

    tasks completed

---
+++ Advanced C#/Regular Expressions - Lab/03. Non-Digit Count/Startup.cs
@@ -0,0 +1,18 @@
+﻿namespace _03.Non_Digit_Count
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+
+    public class Startup
+    {
+        static void Main()
+        {
+            string text = "In 1519 Leonardo da Vinci died at the age of 67.";//Console.ReadLine();
+            string pattern = "[^0-9]";
+
+            int count = Regex.Matches(text, pattern, RegexOptions.IgnoreCase).Count;
+            Console.WriteLine($"Non-digits: {count}");
+        }
+    }
+}
