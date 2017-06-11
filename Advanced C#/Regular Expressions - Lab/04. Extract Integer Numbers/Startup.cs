8a65bb1e7dfef4775e81f897e7b37f4ef2dd082c
PlamenHP <p.petrov.mail@gmail.com>
2017-06-11 19:45:30 +0100

    task completed

--- Advanced C#/Regular Expressions - Lab/04. Extract Integer Numbers/Startup.cs
+++ Advanced C#/Regular Expressions - Lab/04. Extract Integer Numbers/Startup.cs
@@ -1,14 +1,13 @@
 ﻿namespace _04.Extract_Integer_Numbers
 {
     using System;
-    using System.Collections.Generic;
     using System.Text.RegularExpressions;
 
     public class Startup
     {
         static void Main()
         {
-            string text = "In 1519 Leonardo da Vinci died at the age of 67.";
+            string text = Console.ReadLine();
             string pattern = "[0-9]+";
 
             var matches = Regex.Matches(text, pattern, RegexOptions.IgnoreCase);
