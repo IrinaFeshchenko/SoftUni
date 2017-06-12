c0037b48a05974b2e6e53cd7112421f40c55f1b8
PlamenHP <p.petrov.mail@gmail.com>
2017-06-13 00:06:24 +0100

    task completed

---
+++ Advanced C#/Regular Expressions - Lab/08. Extract Quotations/Startup.cs
@@ -0,0 +1,22 @@
+﻿namespace _08.Extract_Quotations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+
+    public class Startup
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex("('|\")(.*?)\\1");
+            var matches = regex.Matches(input);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Groups[2].Value);
+            }
+        }
+    }
+}
