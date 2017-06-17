1fd925dc119be9b9647c80223ba6f4d3eb91153b
PlamenHP <p.petrov.mail@gmail.com>
2017-06-17 11:19:09 +0100

    task completed

---
+++ Advanced C#/Functional Programming - Lab/01. Sort Even Numbers/Startup.cs
@@ -0,0 +1,13 @@
+﻿namespace _01.Sort_Even_Numbers
+{
+    using System;    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Startup    {        public static void Main()        {            int[] nums = Console.ReadLine()
+                                    .Split(new[] { ',',' ' }, StringSplitOptions.RemoveEmptyEntries)
+                                    .Select(int.Parse)
+                                    .ToArray();
+
+            nums.Where(x => x % 2 == 0).OrderBy(x => x).ToList().ForEach(x => Console.Write($"{x}, "));
+        }    }
+}
