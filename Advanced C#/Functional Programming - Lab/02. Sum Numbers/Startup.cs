1a9bd0b1cc90029ea4ee3ffaa82c40c5368a8abe
PlamenHP <p.petrov.mail@gmail.com>
2017-06-17 11:27:22 +0100

    task completed

--- Advanced C#/Functional Programming - Lab/02. Sum Numbers/Startup.cs
+++ Advanced C#/Functional Programming - Lab/02. Sum Numbers/Startup.cs
@@ -1,13 +1,13 @@
-﻿namespace _01.Sort_Even_Numbers
+﻿namespace _02.Sum_Numbers
 {
     using System;    using System.Linq;
 
     public class Startup    {        public static void Main()        {            int[] nums = Console.ReadLine()
-                                    .Split(new[] { ',',' ' }, StringSplitOptions.RemoveEmptyEntries)
+                                    .Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
                                     .Select(int.Parse)
                                     .ToArray();
 
-            var evenNumbers = nums.Where(x => x % 2 == 0).OrderBy(x => x);
-            Console.Write(  string.Join(", ", evenNumbers));
+            Console.WriteLine(nums.Count());
+            Console.WriteLine(nums.Sum());
         }    }
 }
