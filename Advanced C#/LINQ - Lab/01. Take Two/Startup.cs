3bceec983a64017e550b71c4643a26e117d85870
PlamenHP <p.petrov.mail@gmail.com>
2017-06-19 01:15:10 +0100

    task completed

---
+++ Advanced C#/LINQ - Lab/01. Take Two/Startup.cs
@@ -0,0 +1,16 @@
+﻿namespace _01.Take_Two
+{
+    using System;    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Startup    {        public static void Main()        {            int[] nums = Console.ReadLine()
+                                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                                    .Select(int.Parse)
+                                    .Where(x => x >= 10 && x <= 20)
+                                    .Distinct()
+                                    .Take(2)
+                                    .ToArray();
+
+            Console.WriteLine(string.Join(" ",nums));
+        }    }
+}
