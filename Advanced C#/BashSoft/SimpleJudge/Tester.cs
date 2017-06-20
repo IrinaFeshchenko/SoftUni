1914be77f8ca6855264e48903b9210ccf4b62e02
PlamenHP <p.petrov.mail@gmail.com>
2017-06-20 22:21:35 +0100

    Find mismatches of two files line by line

--- Advanced C#/BashSoft/SimpleJudge/Tester.cs
+++ Advanced C#/BashSoft/SimpleJudge/Tester.cs
@@ -1,12 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SimpleJudge
+﻿namespace SimpleJudge
 {
-    class Tester
+    using BashSoft;
+    using System;
+    using System.IO;
+
+    public static class Tester
     {
+        public static void CompareContent(string userOutputPath, string expectedOutputPath)
+        {
+            OutputWriter.WriteMessageOnNewLine("Reading files...");
+
+            string mismatchPath = GetMismatchPath(expectedOutputPath);
+
+            string[] actualOutputLines = File.ReadAllLines(userOutputPath);
+            string[] expectedOutputLines = File.ReadAllLines(expectedOutputPath);
+
+            bool hasMismatch;
+            string[] mismatches = GetLinesWithPossibleMismatches(actualOutputLines, expectedOutputLines, out hasMismatch);
+
+            PrintOutput(mismatches, hasMismatch, mismatchPath);
+            OutputWriter.WriteMessageOnNewLine("Files read!");
+        }
+
+        private static string[] GetLinesWithPossibleMismatches(string[] actualOutputLines, string[] expectedOutputLines, out bool hasMismatch)
+        {
+            hasMismatch = false;
+            string output = string.Empty;
+
+            string[] mismatches = new string[actualOutputLines.Length];
+            OutputWriter.WriteMessageOnNewLine("Comparing files...");
+
+            for (int index = 0; index < actualOutputLines.Length; index++)
+            {
+                string actualLine = actualOutputLines[index];
+                string expectedLine = expectedOutputLines[index];
+
+                if (!actualLine.Equals(expectedLine))
+                {
+                    output = $"Mismatch at line {index} -- expected: \"{expectedLine}\", actual: \"{actualLine}\"";
+                    output += Environment.NewLine;
+                    hasMismatch = true;
+                }
+                else
+                {
+                    output = actualLine;
+                    output += Environment.NewLine;
+                }
+
+                mismatches[index] = output;
+            }
+
+            return mismatches;
+        }
+
+        private static string GetMismatchPath(string expectedOutputPath)
+        {
+            int indexOf = expectedOutputPath.LastIndexOf('\\');
+            string directoryPath = expectedOutputPath.Substring(0, indexOf);
+            string finalPath = directoryPath + @"\Mismatches.txt";
+            return finalPath;
+        }
     }
 }
