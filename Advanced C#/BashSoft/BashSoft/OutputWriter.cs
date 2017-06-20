8f50afca24c4a367cf94021d4928f5cc75fad9d0
PlamenHP <p.petrov.mail@gmail.com>
2017-06-20 21:04:55 +0100

    Displaying a student entry

--- Advanced C#/BashSoft/BashSoft/OutputWriter.cs
+++ Advanced C#/BashSoft/BashSoft/OutputWriter.cs
@@ -1,6 +1,7 @@
 ﻿namespace BashSoft
 {
     using System;
+    using System.Collections.Generic;
 
     public static class OutputWriter
     {
@@ -8,20 +9,28 @@
         {
             Console.Write(message);
         }
+
         public static void WriteMessageOnNewLine(string message)
         {
             Console.WriteLine(message);
         }
+
         public static void WriteEmptyLine()
         {
             Console.WriteLine();
         }
+
         public static void DisplayException(string message)
         {
             ConsoleColor currentColor = Console.ForegroundColor;
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine(message);
             Console.ForegroundColor = currentColor;
+        }
+
+        public static void PrintStudent(KeyValuePair<string, List<int>> student)
+        {
+            OutputWriter.WriteMessageOnNewLine($"{student.Key} - {string.Join(", ",student.Value)}");
         }
     }
 }
