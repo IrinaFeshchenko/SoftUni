e035ae101b2daa4b6f8cd71fbdd062258fa4b422
PlamenHP <p.petrov.mail@gmail.com>
2017-06-28 19:14:19 +0300

    task comleted

---
+++ OOP Basics/Defining Classes - Lab/Defining Classes - Lab/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        BankAccount acc = new BankAccount();
+
+        acc.ID = 1;
+        acc.Balance = 15;
+
+        System.Console.WriteLine($"Account {acc.ID}, balance {acc.Balance}");
+    }
+}
+
