5eefb3e9f2c67a888a50c427377bde5f9375e59f
PlamenHP <p.petrov.mail@gmail.com>
2017-07-09 09:17:29 +0100

    tasks completed

---
+++ OOP Basics/Inheritance - Exercise/Online Radio Database/Startup.cs
@@ -0,0 +1,16 @@
+﻿namespace Online_Radio_Database
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Startup
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
