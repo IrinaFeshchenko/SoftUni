daa5309bc3aa1f528cb4d139ff4a259e2b66351e
PlamenHP <p.petrov.mail@gmail.com>
2017-07-30 18:33:08 +0100

    tasks completed

---
+++ OOP Advanced/Enumerations and Attributes - Exercise/Cards/Card.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+public class Card:IComparable<Card>
+{
+    public Card(string rank, string suit)
+    {
+        Rank = (Rank)Enum.Parse(typeof(Rank),rank);
+        Suit = (Suit)Enum.Parse(typeof(Suit),suit);
+    }
+
+    public int Power => (int)this.Rank + (int)this.Suit;
+
+    public string Name => $"{this.Rank} of {this.Suit}";
+
+    public Rank Rank { get;private set; }
+
+    public Suit Suit { get;private set; }
+
+    public override string ToString()
+    {
+        return $"Card name: {this.Name}; Card power: {this.Power}";
+    }
+
+    public int CompareTo(Card other)
+    {
+        return this.Power.CompareTo(other.Power);
+    }
+}
---
+++ OOP Advanced/Enumerations and Attributes - Exercise/Cards/StartUp.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Cards
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            //CardSuit();
+            //CardRank();
+            //CardPower();
+            CardCompareTo();
+        }
+
+        private static void CardCompareTo()
+        {
+            var first = ReadCard();
+            var second = ReadCard();
+
+            if (first.CompareTo(second)>0)
+            {
+                Console.WriteLine(first);
+            }
+            else
+            {
+                Console.WriteLine(second);
+            }
+        }
+
+        private static Card ReadCard()
+        {
+            string rank = Console.ReadLine();
+            string suit = Console.ReadLine();
+
+            return new Card(rank, suit);
+        }
+
+        private static void CardPower()
+        {
+            string rank = Console.ReadLine();
+            string suit = Console.ReadLine();
+
+            var card = new Card(rank,suit);
+            Console.WriteLine(card.ToString());
+        }
+
+        private static void CardRank()
+        {
+            var input = Console.ReadLine();
+            Console.WriteLine($"{input}:");
+
+            foreach (var value in Enum.GetValues(typeof(Rank)))
+            {
+                Console.WriteLine($"Ordinal value: {(int)value}; Name value: {value}");
+            }
+        }
+
+        public static void CardSuit()
+        {
+            var input = Console.ReadLine();
+
+            Console.WriteLine($"{input}:");
+
+            foreach (var value in Enum.GetValues(typeof(Suit)))
+            {
+                Console.WriteLine($"Ordinal value: {(int)value}; Name value: {value}");
+            }
+        }
+    }
+}
