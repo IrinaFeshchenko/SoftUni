f50d8e5fdf291a99f58e5ae3381f975b33f04ab2
PlamenHP <p.petrov.mail@gmail.com>
2017-08-29 20:23:34 +0100

    added constructors tests

---
+++ OOP Advanced/BashSoft/BashSoftTesting/OrderedDataStructureTester.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+
+namespace BashSoftTesting
+{
+    using System;
+
+    using BashSoft.Contracts;
+    using BashSoft.DataStructures;
+
+    [TestFixture]
+    public class OrderedDataStructureTester
+    {
+        private ISimpleOrderedBag<string> names;
+
+        [Test]
+        public void TestEmptyCtor()
+        {
+            this.names = new SimpleSortedList<string>();
+            Assert.AreEqual(this.names.Capacity, 16);
+            Assert.AreEqual(this.names.Size, 0);
+        }
+
+        [Test]
+        public void TestCtorwithInitialCapacity()
+        {
+            this.names = new SimpleSortedList<string>(20);
+            Assert.AreEqual(this.names.Capacity, 20);
+            Assert.AreEqual(this.names.Size, 0);
+        }
+
+        [Test]
+        public void TestCtorWithAllParams()
+        {
+            this.names = new SimpleSortedList<string>(StringComparer.OrdinalIgnoreCase, 30);
+            Assert.AreEqual(this.names.Capacity, 30);
+            Assert.AreEqual(this.names.Size, 0);
+        }
+
+        [Test]
+        public void TestCtorWithInitialComparer()
+        {
+            this.names = new SimpleSortedList<string>(StringComparer.OrdinalIgnoreCase);
+            Assert.AreEqual(this.names.Capacity, 16);
+            Assert.AreEqual(this.names.Size, 0);
+        }
+    }
+}
