8767c543295b028947932b441a04816636e44261
PlamenHP <p.petrov.mail@gmail.com>
2017-09-30 23:07:37 +0100

    added data context users model and custom attribute

---
+++ C# Web Basics/intro to .NET Core and EF Core Exercise/SocialNetwork/SocialNetwork.Data/SocialNetworkDbContext.cs
@@ -0,0 +1,54 @@
+﻿namespace SocialNetwork.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Infrastructure;
+
+    public class SocialNetworkDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer(@"Server = COMP\SQLEXPRESS; Database = SocialNetworkDb; Integrated Security = true");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+        }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            var serviceProvider = this.GetService<IServiceProvider>();
+
+            var items = new Dictionary<object, object>();
+
+            foreach (var entry in this.ChangeTracker.Entries().Where(e => (e.State == EntityState.Added) || (e.State == EntityState.Modified)))
+                {
+                    var entity = entry.Entity;
+
+                    var context = new ValidationContext(entity, serviceProvider, items);
+
+                    var results = new List<ValidationResult>();
+
+                if (Validator.TryValidateObject(entity, context, results, true) == false)
+                {
+                    foreach (var result in results)
+                    {
+                        if (result != ValidationResult.Success)
+                        {
+                            throw new ValidationException(result.ErrorMessage);
+                        }
+                    }
+                }
+            }
+
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+    }
+}
---
+++ C# Web Basics/intro to .NET Core and EF Core Exercise/SocialNetwork/SocialNetwork/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SocialNetwork
+{
+    using Microsoft.EntityFrameworkCore;
+    using SocialNetwork.Data;
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
