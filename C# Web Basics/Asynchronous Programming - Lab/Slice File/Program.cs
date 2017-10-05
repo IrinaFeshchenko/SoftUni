6116128fb3037ab203fa28c00f30db7fad636f7e
PlamenHP <p.petrov.mail@gmail.com>
2017-10-05 19:10:11 +0100

    added even numbers

---
+++ C# Web Basics/Asynchronous Programming - Lab/Slice File/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Slice_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
