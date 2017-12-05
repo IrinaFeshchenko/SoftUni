b223aa4a07c1d0a1a0cc71d58422f1189ce63c10
PlamenHP <p.petrov.mail@gmail.com>
2017-12-05 00:20:36 +0000

    Initial app structure

---
+++ C# MVC/Teamwork/Teamwork.Data/Models/Group.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Teamwork.Data.Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+
+        public User Creator { get; set; }
+
+        public string Introduction { get; set; }
+        
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public DateTime LateSubmisionDate { get; set; }
+
+
+    }
+}
---
+++ C# MVC/Teamwork/Teamwork.Data/Models/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Teamwork.Data.Models
+{
+    // Add profile data for application users by adding properties to the User class
+    public class User : IdentityUser
+    {
+    }
+}
