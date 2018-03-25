b0b5c18818685eb15b80a72cc7e8174877bc9d97
PlamenHP <p.petrov.mail@gmail.com>
2018-03-25 05:25:57 +0100

    phonebook

---
+++ JS Apps/Phonebook/phonebook.js
@@ -0,0 +1,60 @@
+﻿$(function () {
+    $('#btnLoad').click(loadContacts);
+    $('#btnCreate').click(createContact);
+    const baseUrl = 'https://phonebook-194ba.firebaseio.com/phonebook';
+
+    function createContact() {    
+        let contact = {
+            person: $('#person').val(),
+            phone: $('#phone').val()
+        };
+        let createRequest = {
+            method: 'POST',
+            url: baseUrl + '.json',
+            data: JSON.stringify(contact),
+        }
+        $.ajax(createRequest)
+            .then(loadContacts)
+            .catch(displayError);
+    }
+
+    function loadContacts() {
+        $('#phonebook').empty();
+        $.get(baseUrl + '.json')
+            .then(displayContacts)
+            .catch(displayError);
+    }
+
+    function displayContacts(contacts) {
+        let keys = Object.keys(contacts);
+
+        for (let key of keys) {
+            let contact = contacts[key];
+            if (contact.person) {
+                let li = $('<li>');
+                li.text(contact.person + ':' + contact.phone);
+                li.appendTo($('#phonebook'));
+                li.append(' ');
+                li.append(
+                    $('<a href="#">[Delete]</a>').click(function () {
+                        deleteContact(key);
+                    })
+                );
+            }
+        }
+    }
+
+    function displayError() {
+        $('#phonebook').append('<li>Error</li>');
+    }
+
+    function deleteContact(key) {
+        let delRequest = {
+            method: "DELETE",
+            url: baseUrl + '/' + key + ".json"
+        };
+        $.ajax(delRequest)
+            .then(loadContacts)
+            .catch(displayError);
+    }
+});
