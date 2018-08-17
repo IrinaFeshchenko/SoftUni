ba02b35c5f349e84adc8617496f4b15816baf8d4
PlamenHP <p.petrov.mail@gmail.com>
2018-08-18 02:09:26 +0300

    adding db models

---
+++ C# MVC/SocialApp/SocialApp/Data/Models/Reply.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialApp.Data.Models
+{
+    public class Reply
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        [Required]
+        public DateTime replyDate { get; set; }
+
+        [Required]
+        public int TopicId { get; set; }
+
+        public Topic Topic { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        public User User { get; set; }
+    }
+}
---
+++ C# MVC/SocialApp/SocialApp/Data/Models/Topic.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialApp.Data.Models
+{
+    public class Topic
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Subject { get; set; }
+
+        [Required]
+        public DateTime CreatedOn { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        public User User { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+
+        public List<Reply> Replies { get; set; }
+    }
+}
