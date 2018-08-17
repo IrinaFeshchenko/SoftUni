ebb623789f98dc9ec130e9c40e4eeeb9c8e4ab41
PlamenHP <p.petrov.mail@gmail.com>
2018-08-18 00:55:04 +0300

    initial commit

---
+++ C# MVC/SocialApp/SocialApp/Data/SocialAppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SocialApp.Data
+{
+    public class SocialAppDbContext : IdentityDbContext
+    {
+        public SocialAppDbContext(DbContextOptions<SocialAppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
