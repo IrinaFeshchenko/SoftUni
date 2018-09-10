4e4736888a75c92851cf698148621934d6fca56e
PlamenHP <p.petrov.mail@gmail.com>
2018-09-11 00:47:56 +0100

    adding signalr

--- C# MVC/SocialApp/Chat/Hubs/ChatHub.cs
+++ C# MVC/SocialApp/Chat/Hubs/ChatHub.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,9 +9,27 @@ namespace Chat.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+		public static ConcurrentDictionary<string, string> ConnectedUsers = new ConcurrentDictionary<string, string>();
+
+		public async override Task OnConnectedAsync()
+		{
+			string connectionId = Context.ConnectionId;
+			string userName = Context.User.Identity.Name;
+
+			ConnectedUsers.TryAdd(connectionId, userName);
+			await base.OnConnectedAsync();
+		}
+
+		public async override Task OnDisconnectedAsync(Exception ex)
+		{
+			ConnectedUsers.TryRemove(Context.ConnectionId, out string result);
+
+			await base.OnDisconnectedAsync(ex);
+		}
+		public async Task SendMessage(string user, string message, string avatar, string friends)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync("ReceiveMessage", user, message, avatar);
+
         }
     }
 }
--- C# MVC/SocialApp/Chat/wwwroot/js/chat.js
+++ C# MVC/SocialApp/Chat/wwwroot/js/chat.js
@@ -1,24 +1,82 @@
-﻿"use strict";
+﻿$(document).ready(() => {
 
-var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+	var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
-connection.on("ReceiveMessage", function (user, message) {
-    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    var encodedMsg = user + " says " + msg;
-    var li = document.createElement("li");
-    li.textContent = encodedMsg;
-    document.getElementById("messagesList").appendChild(li);
-});
+	connection.on("ReceiveMessage", function (user, message, avatar) {
+		var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+		var li = '<li class="chatMessage"><img src="' + avatar + '" alt="Image" class="messageAvatar" /><div class="message">' + message + '<div></li>';
+		$('#messages').prepend(li);
+	});
 
-connection.start().catch(function (err) {
-    return console.error(err.toString());
-});
+	connection.start().catch(function (err) {
+		return console.error(err.toString());
+	});
 
-document.getElementById("sendButton").addEventListener("click", function (event) {
-    var user = document.getElementById("userInput").value;
-    var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessage", user, message).catch(function (err) {
-        return console.error(err.toString());
-    });
-    event.preventDefault();
+	$('.chatContact').on('click', 'div', function () {
+		let selectedProfileId = this.id;
+
+		$.ajax({
+			type: 'POST',
+			url: 'Profile/AjaxGetProfileInfo',
+			dataType: "json",
+			data: { selectedId: selectedProfileId },
+			success: successFunc,
+			error: errorFunc
+		});
+
+		function successFunc(data) {
+			$('#profileImage').attr('src', data.avatar);
+			$('#profileName').text('Name: ' + data.name);
+			$('#profileEmail').text('Email: ' + data.email);
+			$('#profileRegisteredOn').text('Registered: ' + data.registeredOn);
+			$('#profileContactsCount').text('Contacts: ' + data.contactsCount);
+			$('#profileInfoPanel').removeClass('hidden');
+			$('#FriendId').val(selectedProfileId);
+		}
+
+		function errorFunc() {
+		}
+	});
+
+	$('#removeContact').on('click', function () {
+		let friendId = $('#FriendId').val();
+		let userId = Model_Id;
+
+		$.ajax({
+			type: 'POST',
+			url: 'Profile/AjaxRemoveContact',
+			dataType: "json",
+			data: { contactId: friendId, userId: userId },
+			success: successFunc,
+			error: errorFunc
+		});
+
+		function successFunc(data) {
+			$('#profileInfoPanel').addClass('hidden');
+			$('#' + friendId).parent().remove();
+		}
+
+		function errorFunc() {
+		}
+
+	});
+
+	$('#send-message').on('click', function () {
+
+		let message = $('#message').val();
+		let user = Model_NickName;
+		let avatar = Model_Avatar;
+
+		$('#message').val('');
+
+		if (message) {
+
+			$('#messages').prepend('<li class="chatMessage"><img src="' + avatar +'" alt="Image" class="messageAvatar" /><div class="message">' + message + '<div></li>');
+
+			connection.invoke("SendMessage", user, message, avatar, '').catch(function (err) {
+				return console.error(err.toString());
+			});
+			event.preventDefault();
+		}
+	});
 });
